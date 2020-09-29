89440d4414d768b7999be9f5d53d9fa8f1e33ef1
anujsoni08 <anujsoni08@gmail.com>
2020-09-30 03:00:16 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux"; // Add provider to make redux store available to project
+import "bootstrap/dist/css/bootstrap.css"; // Added bootstrap
 
-import albumListReducer from "./store/reducers/albumList";
-import albumItemReducer from "./store/reducers/albumItem";
+import albumListReducer from "./store/reducers/albumList"; // Albumlist reducer
+import albumItemReducer from "./store/reducers/albumItem"; // AlbumItem reducer
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -17,7 +17,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // using chrome extension to track state in browser
 );
 
 ReactDOM.render(
